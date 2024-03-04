11ed20c3a5a580454d6f428b1c312570c8c0a5b3
Raul Torres <torresraulp@icloud.com>
2024-03-04 18:52:44 -0300

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    
+    $('#cpf').mask('000.000.000-00')
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cep').mask('00000-000')
+
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome completo'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+})
